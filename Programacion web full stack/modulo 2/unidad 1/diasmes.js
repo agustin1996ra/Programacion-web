8423d8a941864428ae8126cd269e621da8f3a93e
Agustin   Rodriguez Alvarez <63318331+agustin1996ra@users.noreply.github.com>
2020-12-31 03:22:09 -0300

    30.12

---
+++ Programacion web full stack/modulo 2/unidad 1/diasmes.js
@@ -0,0 +1,7 @@
+var meses =["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+var diasDelMes = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+for(let i = 0; i < 12; i++){
+    console.log("El mes " + meses[i] + " tiene " + diasDelMes[i] + " dias.")
+}
